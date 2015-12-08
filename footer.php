9cb88964d773b588bb672edcdcc505dbe649c826
Kevin McCartney <hello@kevinmccartney.is>
2015-12-07 23:30:01 -0800

    fixed footer

--- footer.php
+++ footer.php
@@ -1,54 +1,49 @@
 <?php do_action( 'hji_theme_before_footer' ); ?>
-
 <footer id="footer" class="content-info" role="contentinfo">
 
-		<div class="container">
+  <div class="container">
 
-            <div class="line hidden-xs">
-            
-			  <div class="footer-menu-wrapper">
-			  	<?php
-            		if ( has_nav_menu( 'footer-menu' ) ) :
-                		wp_nav_menu( array( 'theme_location' => 'footer-menu', 'menu_class' => 'nav nav-pills' ) );
-            	endif;
-            	?>
-			  </div>
+    <div class="footer-menu-wrapper">
 
-            </div>
+      <?php
+      if ( has_nav_menu( 'footer-menu' ) ) :
+      wp_nav_menu( array( 'theme_location' => 'footer-menu', 'menu_class' => 'nav nav-pills' ) );
+      endif;
+      ?>
 
-            <?php do_action( 'hji_theme_before_footer_widgets' ); ?>
+    </div>
 
-			<?php if ( is_active_sidebar( 'blvd-footerwidgets' ) ) : ?>
+    <?php do_action( 'hji_theme_before_footer_widgets' ); ?>
 
-				<div class="footer-widgets row">
+    <?php if ( is_active_sidebar( 'blvd-footerwidgets' ) ) : ?>
 
+    <div class="footer-widgets row">
 
-					<?php dynamic_sidebar( 'blvd-footerwidgets' ); ?>
+      <?php dynamic_sidebar( 'blvd-footerwidgets' ); ?>
 
-				</div>
+    </div>
 
-			<?php endif; ?>
+    <?php endif; ?>
 
-			<?php do_action( 'hji_theme_after_footer_widgets' ); ?>	    	
+    <?php do_action( 'hji_theme_after_footer_widgets' ); ?>
 
-			<?php do_action( 'hji_theme_above_copyright' ); ?>
+    <?php do_action( 'hji_theme_above_copyright' ); ?>
 
+  </div>
 
-		</div>
+  <div id="copyright" class="fineprint">
 
-		<div id="copyright" class="fineprint">
+    <div class="container">
 
-			<div class="container">
+      <span class="copyright"><?php hji_footer_copyright(); ?></span>
 
-				<span class="copyright"><?php hji_footer_copyright(); ?></span>
+      <span class="hji-powered"><a href="http://www.homejunction.com/" target="_blank">Real Estate Website Design</a> powered by Home Junction.</span>
 
-				<span class="hji-powered"><a href="http://www.homejunction.com/" target="_blank">Real Estate Website Design</a> powered by Home Junction.</span>
+    </div>
 
-			</div>
+  </div>
 
-		</div>
-
-<?php wp_footer(); ?>
+  <?php wp_footer(); ?>
 
 </footer>
 
