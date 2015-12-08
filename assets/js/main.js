eb6d754fb8ef4ccaaffd903f0675e4a285eab314
Kevin McCartney <hello@kevinmccartney.is>
2015-12-08 00:27:57 -0800

    added js files

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+partnerImg = jQuery('.partner-img-wrapper img'),
+partnerPod = jQuery('.partner-pod');
+
+function partnerWellSizer() {
+    maxImgHeight = null;
+    maxPodHeight = null;
+
+    jQuery(partnerImg).each(function() {
+        partnerImgHeight = jQuery(this).outerHeight(true);
+        maxImgHeight = (partnerImgHeight > maxImgHeight) ? partnerImgHeight : maxImgHeight;
+    });
+
+    jQuery(partnerPod).each(function() {
+        partnerPodHeight = jQuery(this).outerHeight(true);
+        maxPodHeight = (partnerPodHeight > maxPodHeight) ? partnerPodHeight : maxPodHeight;
+    });
+
+    if (maxPodHeight <= maxImgHeight) {
+        jQuery(partnerPod).css("height", maxImgHeight * 1.25);
+    } else if (maxPodHeight >= maxImgHeight) {
+        jQuery(partnerPod).css("height", maxPodHeight);
+    } else {
+        // nothing!
+    }
+}
+
+function agentPolyfill() {
+    var agentImgHeight = jQuery('.post-type-archive .agent-roster-photo img').outerHeight(true),
+        agentBoxHeight = jQuery('.post-type-archive .agent-data').outerHeight(true);
+
+    if (agentImgHeight >= agentBoxHeight) {
+        jQuery('.post-type-archive .agent-data').css('height', agentImgHeight * 1.25);
+    } else {
+        // nothing!
+    }
+}
+
+jQuery(document).ready(function() {
+    agentPolyfill();
+    partnerWellSizer();
+});
+
+jQuery(window).resize(function() {
+    jQuery('.partner-pod').css('height', '');
+    partnerWellSizer();
+});
--- functions.php
+++ functions.php
@@ -1,27 +1,16 @@
 <?php
 /**
- * Dawn Houlf Child Theme Functions
- *
- * These are primarily sample functions for building a child theme.
- * You should remove or modify everthing below to suit your needs.
- */
-
-/**
  * Sets our SCSS constant for the improved build process
  * REQUIRED
  *
  * @since 2.7.0
  */
 if ( !defined( 'HJI_BLVD_SCSS' ) ) {
-
     define( 'HJI_BLVD_SCSS', true );
-
 }
 
-if ( !function_exists( 'hji_widgets_child_init' ) ) :
-
+if ( !function_exists( 'hji_widgets_child_init' ) ) {
     function hji_widgets_child_init() {
-
         register_sidebar( array(
             'id'            => 'hjitw-home-first-widgets',
             'name'          => __( 'Home Page First Widget', 'hji_themework' ),
@@ -31,7 +20,6 @@ if ( !function_exists( 'hji_widgets_child_init' ) ) :
             'before_title'  => '<div class="widget-title"><h2>',
             'after_title'   => '</h2></div>',
         ));
-
         register_sidebar( array(
             'id'            => 'hjitw-home-second-widgets',
             'name'          => __( 'Home Page Second Widget', 'hji_themework' ),
@@ -41,7 +29,6 @@ if ( !function_exists( 'hji_widgets_child_init' ) ) :
             'before_title'  => '<div class="widget-title"><h2>',
             'after_title'   => '</h2></div>',
         ));
-
         register_sidebar( array(
             'id'            => 'hjitw-home-third-widgets',
             'name'          => __( 'Home Page Third Widget', 'hji_themework' ),
@@ -51,10 +38,13 @@ if ( !function_exists( 'hji_widgets_child_init' ) ) :
             'before_title'  => '<div class="widget-title"><h2>',
             'after_title'   => '</h2></div>',
         ));
-
-
+        unregister_sidebar( 'idx-horizontal-search');
+        unregister_sidebar( 'blvd-main-sidebarwidgets');
+        unregister_sidebar( 'blvd-footerwidgets');
+        unregister_sidebar( 'blvd-homewidgets');
     }
-
     add_action( 'widgets_init', 'hji_widgets_child_init' );
+}
 
-endif;
+ wp_register_script( 'hji_dawn-houlf-scripts', HJI_THEME_ASSETS_URL . '/js/main.js', array( 'jquery' ), false, true );
+ wp_enqueue_script( 'hji_dawn-houlf-scripts' );
