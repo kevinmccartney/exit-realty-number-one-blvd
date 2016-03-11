537ee8866c420ac5f32cbf7260ce83620159d634
Kevin McCartney <hello@kevinmccartney.is>
2016-03-11 13:48:27 -0800

    took scripts out of theme for debugging

--- functions.php
+++ functions.php
@@ -45,11 +45,3 @@ if (!function_exists('hji_widgets_child_init')) {
     }
     add_action('widgets_init', 'hji_widgets_child_init', 11);
 }
-
-
-if (!function_exists('hji_dawn_houlf_enqueue_scripts')) {
-    function hji_dawn_houlf_enqueue_scripts() {
-        wp_enqueue_script( 'hji_dawn_houlf_scripts', get_stylesheet_directory_uri() . '/assets/js/main.js', array( 'jquery' ), null, true );
-    }
-    add_action('wp_enqueue_scripts', 'hji_dawn_houlf_enqueue_scripts', 11);
-}
